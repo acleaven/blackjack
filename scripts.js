c22220fb1c118809238cdb1b79c6766779348b60
CBonk <Chris.W.Bonk@gmail.com>
2017-11-05 23:25:20 -0500

    Update scripts.js
    Just a couple of updates:
    1. I added a "+1" to "var randomIndex" in order to round up the answer to a whole number.
    2.

--- scripts.js
+++ scripts.js
@@ -73,7 +73,7 @@ function Deck(cardArray) {
     this.cardArray = [C2, C3, C4, C5, C6, C7, C8, C9, C10, CJ, CQ, CK, CA, D2, D3, D4, D5, D6, D7, D8, D9, D10, DJ, DQ, DK, DA, H2, H3, H4, H5, H6, H7, H8, H9, H10, HJ, HQ, HK, HA, S2, S3, S4, S5, S6, S7, S8, S9, S10, SJ, SQ, SK, SA];
 
     this.deal = function(e) {
-        var randomIndex = Math.floor((Math.random() * 52); //this probably needs to be made a function that gets called repeatedly otherwise we'll be calling the same random number repeatedly.
+        var randomIndex = Math.floor((Math.random() * 52 + 1); //this probably needs to be made a function that gets called repeatedly otherwise we'll be calling the same random number repeatedly.
         var randomCard = cardArray[randomIndex];
         var dealButton = document.getElementById('deal');
         var hitButton = document.getElementById('hit'):
@@ -110,6 +110,8 @@ function Player(bankValue, betValue, handArray, handValue) {
         }
     }
 }
+
+
 
 //Constructor for Dealer object:
 function Dealer(handValue, handArray) {
